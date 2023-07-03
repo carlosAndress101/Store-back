2d70027047ad8c1304a9111512586e186f07584e
carlosAndress101 <carlos.hinestrozap@unac.edu.co>
2023-07-02 18:55:41 -0700

    function reload

--- index.js
+++ index.js
@@ -7,3 +7,12 @@ const port = process.env.PORT || 3000;
 app.listen(port, ()=>{
   console.log(`Server on port: ${port}`)
 })
+
+// Mantener el proceso en ejecución
+function keepAlive() {
+  setInterval(() => {
+    console.log('El backend sigue en ejecución...');
+  }, 15000);
+}
+
+keepAlive();
